fa3294328dcc2ebde324b4a103a5860020e3165b
wanloq <mcjinkz@gmail.com>
2026-08-14 15:31:52 +0100

    Enhance Activity 8 Tip Calculator: refactor functions for clarity, add split bill functionality, and update index.html for improved layout and button interactions.

--- activity8.js
+++ activity8.js
@@ -15,20 +15,31 @@ what each one is responsible for, before writing code.
 Step 6 — Write the Code: Declare your functions with parameters, then call them with 
 test values. 
 Step 7 — Test the Program 
-Part 5: Extension Challenge 
-Add a third function that calculates the total bill split evenly between a given number 
-of people  
 
  */
 
-// Tip calculator Program
+
+
+// Tip calculator function
 /* 
 Input (parameters): amount($), tip(%)
 Processing: total = %increase(amount*(tip/100))
 Output (return): totalTip($) 
 */
+const output = document.querySelector("#output");
+let message = ''
+let amount = 0;
+let tip = 0;
+let shareNum = 0;
+let totalTip = 0;
+let totalBill = 0;
 
-function tipCalculator(amount, tip) {
+
+function tipCalculator() {
+
+    // Input
+    amount = parseFloat(prompt('What is the total spend in $$ (e.g. 150)?'));
+    tip = parseFloat(prompt('What is the acceptable tip in percent (e.g. 15)?'));
 
     // Processing
     const totalTip = amount * (tip / 100);
@@ -40,26 +51,86 @@ function tipCalculator(amount, tip) {
 };
 
 
-// Tip display Program
+
+
+// Tip display function
 /* 
-Input (prompt): amount($), tip(%)
-Processing: totalBill = amount + totalTip
-Output (alert): a formatted message: "For a bill of $(amount) with a (tip)% tip, you should leave $(totalTip). Your total is $115."
+
+Input (prompt): 
+amount($), tip(%)
+get the shared bill from the splitBill function
+
+Processing: 
+totalBill = amount + totalTip
+use if-else block to check which of the tip types the user wants to display
+
+
+Output (): build a formatted messages depending on which of the options (split bill or not) 
+
 */
 
-function tipDisplay() {
+function tipDisplay(split) {
+
+    // check which button the user clicked
+    // if user clicked the button to "split the bill":
+    if (split) {
+
+        // get  the shared bill from the splitBill function
+        let sharedBill = splitBill()
+
+        // build message for split bill function
+        message = `The total bill of $${totalBill} has been successfully divided equally between ${shareNum} people and each person will pay $${sharedBill}`;
+
+    } else {
+        // if user clicked the "calculate tip" button:
+        // get the total tip from the tip calculator function
+        totalTip = tipCalculator();
+        totalBill = amount + totalTip;
+
+        // build message for 
+        message = `For a total spend of $${amount} and a ${tip}% tip, you should leave $${totalTip}. Your Total bill is $${totalBill}`;
+
+    }
+
+
+    output.innerText = message;
+    alert(message)
+
+};
+
+
+/* 
+Split the bill extension
+
+Part 5: Extension Challenge 
+Add a third function that calculates the total bill split evenly between a given number 
+of people  
+
+
+Input (prompt): number of heads, 
+Input (from tipDisplay function): amount($), tip(%)
+
+Processing: 
+totalBill = amount + totalTip
+sharedBill = totalBill/number of heads
+
+Output (alert): return sharedBill
+
+*/
+
+function splitBill() {
 
     // Input
-    const amount = Number(prompt('What is the total spend in $$ (e.g. 150)?'));
-    const tip = prompt('What is the acceptable tip in percent (e.g. 15)?');
+    let totalTip = tipCalculator();
+    shareNum = parseInt(prompt('How many will share the bill (e.g. 3)?'));
 
     // Processing
-    let totalTip = tipCalculator(amount, tip);
     let totalBill = amount + totalTip;
+    let sharedBill = 0;
 
-    // Output
-    let message = `For a total spend of $${amount} and a ${tip}% tip, you should leave $${totalTip}. Your Total bill is $${totalBill}`;
+    // equal share (no rounding)
+    sharedBill = totalBill / shareNum;
 
-    console.log(message);
-    alert(message);
-};
+    return sharedBill;
+
+}
